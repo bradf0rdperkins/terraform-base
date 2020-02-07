4d421f71d55b0643c9454f7d770058caeea2fd96
Bradford L. Perkins <blperkins@omnicloud.local>
2020-02-07 14:32:51 -0700

    updating variables

--- rds.tf
+++ rds.tf
@@ -1,15 +1,15 @@
-resource "aws_db_instance" "default" {
-  allocated_storage          = "${DB_ALLOCATED_STORAGE}"
+resource "aws_db_instance" "fcc-prod-rds-mysql" {
+  allocated_storage          = "${var.DB_ALLOCATED_STORAGE}"
   storage_type               = "gp2"
   engine                     = "mysql"
   engine_version             = "5.7"
   instance_class             = "db.t2.micro"
-  name                       = "${DB_NAME}"
-  identifier                 = "${DB_NAME}"
-  username                   = "${DB_USERNAME}"
-  password                   = "${DB_PASS}"
+  name                       = "${var.DB_NAME}"
+  identifier                 = "${var.DB_NAME}"
+  username                   = "${var.DB_USERNAME}"
+  password                   = "${var.DB_PASS}"
   parameter_group_name       = "default.mysql5.7"
   db_subnet_group_name       = "${aws_db_subnet_group.fcc-acedirect-db-subnet-group.id}"
   auto_minor_version_upgrade = true
-  port                       = "${DB_PORT}"
+  port                       = "${var.DB_PORT}"
 }
