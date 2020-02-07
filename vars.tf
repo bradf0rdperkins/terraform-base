fa2fbc6a978ac7fede2ac12c0ff070fe78c2ccd3
Bradford L. Perkins <blperkins@omnicloud.local>
2020-02-07 13:11:22 -0700

    moving variables

--- vars.tf
+++ vars.tf
@@ -10,3 +10,19 @@ variable "PATH_TO_PUBLIC_KEY" {
   default = "mykey.pub"
 }
 
+variable "ingress_web_ports_1" {
+  default = [22, 443, 8080, 80, 8443, 5060, 3478, 5038, 8005, 3306, 8081]
+}
+variable "ingress_web_ports_2" {
+  default = [3478, 5061, 3306, 5060]
+}
+variable "egress_web_ports_1" {
+  default = [80, 8443, 443, 3478, 5038, 3306]
+}
+variable "egress_web_ports_2" {
+  default = [3478, 443, 50561, 3306]
+}
+variable "protocols" {
+  default = ["tcp", "udp"]
+}
+
